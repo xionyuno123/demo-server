4fb7e294ff16bbdd8b9ba0b47ccd3fae139bd663
duanjp8617 <duanjp8617@gmail.com>
2022-08-10 10:17:17 +0000

    perform a sync

---
+++ test-pmd/src/main.cc
@@ -0,0 +1,14 @@
+
+#include <iostream>
+#include "wtf.h"
+
+using namespace std;
+
+int main(int argc, const char *argv[]) {
+  wtf w;
+  w.i = 0;
+  w.j = 1;
+  std::cout<<"wtf??"<<std::endl;
+  std::cout<<w.i<<std::endl;
+  return 0;
+}
